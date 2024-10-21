c08a7befa5a4a811bf39b57db3e844ddd3f7df20
popstar403 <colinbroyles@gmail.com>
2024-10-21 16:07:17 -0500

    Added division function -- Colin

--- calculator.py
+++ calculator.py
@@ -2,13 +2,10 @@
 #Guest Arf added the subtract function
 def add(a,b):
   print("Result",a+b)
-<<<<<<< HEAD
 def subtract(a,b):
   print("Result",a-b)
-=======
 
 
-def divide(dividend, divisor): #DIvision function, by Colin
+def divide(dividend, divisor): #Dvision function, by Colin
   result = str(dividend/divisor)
   print(f"Result: {result}")
->>>>>>> fc3a9d9 (Added division function -- Colin)
